6faae2befaf810aa7c704b2923c7d3a61dc5295d
Proladon <Proladon@gmail.com>
2024-02-19 21:23:47 +0800

    mod: cmd - info embed datetime footer

--- src/commands/info/embeds.ts
+++ src/commands/info/embeds.ts
@@ -1,13 +1,15 @@
 import { EmbedBuilder, Client } from 'discord.js'
 import gitRev from 'git-rev-sync'
 import { stateColors } from '@/config/colors'
+import { dateFormat } from '@/utils/date'
 
 export default (client: Client<true>) => {
   return new EmbedBuilder()
     .setColor(stateColors.info)
     .setTitle(`${client.user?.username} Info`)
     .addFields(
-      // { name: '\u200B', value: '\u200B' },
+      // { name: '\u200B', value: '\u200B' }  // empty field,
       { name: 'Version', value: gitRev.tag() },
     )
+    .setFooter({ text: dateFormat(dayjs().toDate()) })
 }
